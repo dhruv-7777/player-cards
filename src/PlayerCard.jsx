fbfb40ecca3d6ccb7d9312195119a55f0a6c2b38
Dhruv Patel <dhruv.patel@primedemo.in>
2024-07-17 16:11:17 +0530

    Task 2

--- src/PlayerCard.jsx
+++ src/PlayerCard.jsx
@@ -1,21 +1,3 @@
-// import React from 'react'
-// const PlayerCard = (props) => {
-
-//     const clickHandle = (player) => {
-//         props.onClickEvent(player)
-//     }
-//     return (
-//         <div className="border border-gray-300 rounded-lg p-4 text-center shadow-md bg-white m-4 min-w-[250px] cursor-pointer col-6 col-lg-2" onClick={() => clickHandle(props.player)}>
-//             <div className="w-10 h-10 rounded-full bg-gray-300 mx-auto items-center justify-center text-2xl mb-2">{props.player.name.split(" ")[0][0]}</div>
-//             <h2>{props.player.name}</h2>
-//             <p>{props.player.country}</p>
-//             <p>{props.player.role}</p>
-//         </div>
-//     )
-// }
-
-// export default PlayerCard
-
 import React from 'react';
 import { Typography, Avatar, Paper } from '@mui/material'; // Importing Material UI components
 
